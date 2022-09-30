2bb64c78e7c73392a5dde53d9499533e66b52e68
tokyumilla <tokyumilla@gmail.com>
2022-09-30 19:12:52 +0200

    creo función helloWorld

--- App.java
+++ App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        helloWorld();
     }
+
+
+/**
+ * 
+ */
+
+    public static void helloWorld(){
+        System.out.println("Hello World!");
+    }
+
 }
